e1f5aa07135d66db543722212d0a4ccb83032f64
Maksym Domin <maximd@ya0ne.com>
2020-01-06 18:42:12 +0200

    Nicomachus theorem

--- src/main/java/com/algorithms/simple/MathematicalInduction.java
+++ src/main/java/com/algorithms/simple/MathematicalInduction.java
@@ -1,9 +1,5 @@
 package com.algorithms.simple;
 
-/**
- * Proving 1+3+...(2n-1) = n2
- * @param args
- */
 public class MathematicalInduction {
 	private long n;
 
@@ -12,16 +8,47 @@ public class MathematicalInduction {
 	}
 
 	public static void main(String[] args) {
-		MathematicalInduction mathematicalInduction = new MathematicalInduction(10L);
+		MathematicalInduction mathematicalInduction = new MathematicalInduction(3L);
 		long result = mathematicalInduction.induction();
 		System.out.println(result);
+//		result = mathematicalInduction.nicomachus();
+//		System.out.println(result);
 	}
-	
+
+	/**
+	 * Proving 1+3+...(2n-1) = n(2)
+	 * @return long
+	 */
 	public long induction() {
 		long result = 0;
 		for( long i = 1; i<=n; i++ ) {
 			result += (2*i-1);
 		}
 		return result;
+	}
+
+	/**
+	 * Proving 1(3)+2(3)+3(3)+...n(3) = (1+2+3+...n)(2)
+	 * @return
+	 */
+	public long nicomachus( long element, long power ) {
+		long result_cube = 0;
+		long result_square = 0;
+		int cube = 3;
+//		do {
+//			
+//		} while( nicomachus( i ) );
+		
+		for( long i = 1; i<=n; i++ ) {
+			result_cube += Math.pow(i, cube);
+		}
+		for( long i = 1; i<=n; i++ ) {
+			result_square += i;
+		}
+		result_square = result_square*result_square;
+		if( result_cube == result_square ) {
+			System.out.println(result_cube + " " + result_square);
+		}
+		return result_cube;
 	}
 }
