6f9635c5bc412cd30d827863dca049bc6a7097fd
Maksym Domin <maximd@ya0ne.com>
2020-01-26 22:04:54 +0200

    Doc updated

--- src/main/java/com/algorithms/simple/Sums.java
+++ src/main/java/com/algorithms/simple/Sums.java
@@ -7,11 +7,16 @@ public class Sums {
 	public static void main(String[] args) {
 		Sums sums = new Sums();
 		int numbers[] = new int[]{4, -1, -5, 0, 2, 4, 1, 4, 5, 6};
-		//int numbers[] = new int[]{-1, 0, 1};
 		Pair pair = sums.findElementsWithSumEqualsZero(numbers, 0);
 		System.out.println(pair);
 	}
 
+	/**
+	 * Find two first elements of array sum of which is equals to 0.
+	 * @param numbers - array
+	 * @param element - current element to sum with others
+	 * @return Pair - pair of elements (positions in array)
+	 */
 	public Pair findElementsWithSumEqualsZero(int numbers[], int element) {
 		int a = numbers[element];
 		for( int i = element; i<numbers.length; i++ ) {
